43f0081bbca5309281eba8b8b93076c1a908d052
konstantin.partynin <partinin@bk.ru>
2018-12-16 14:48:26 +0400

    Add method testURL and new field protocol

--- src/Client.java
+++ src/Client.java
@@ -16,14 +16,32 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client extends Application {
 
-    TextField tfServer = new TextField();
-    TextField tfPort = new TextField();
+    private TextField tfProtocol = new TextField();
+    private TextField tfServer = new TextField();
+    private TextField tfPort = new TextField();
+    private ArrayList<String> list = new ArrayList<>(1000);
+    private String server;
+    private String port;
+    private String protocol;
+    private final Pattern pPicture = Pattern.compile(".+\\.(png|gif|jpg)");
+    private final Pattern p = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*" +
+            "[-a-zA-Z0-9+&@#/%=~_|]");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Panel p to hold the label enter a protocol and text field
+        BorderPane paneForLabelAndTfEnterAProtocol = new BorderPane();
+        paneForLabelAndTfEnterAProtocol.setPadding(new Insets(5, 5, 5, 5));
+        paneForLabelAndTfEnterAProtocol.setLeft(new Label("Enter a protocol:  "));
+        tfProtocol.setAlignment(Pos.BOTTOM_RIGHT);
+        tfProtocol.setText("https://"); // Default protocol
+        paneForLabelAndTfEnterAProtocol.setCenter(tfProtocol);
+
         // Panel p to hold the label enter a server and text field
         BorderPane paneForLabelAndTfEnterAServer = new BorderPane();
         paneForLabelAndTfEnterAServer.setPadding(new Insets(5, 5, 5, 5));
@@ -49,8 +67,8 @@ public class Client extends Application {
         // VPanel for hold the fields enter server, enter port and button submit
         VBox vPanelForEnterServerPortAndButton = new VBox();
         vPanelForEnterServerPortAndButton.setStyle("-fx-border-color: green");
-        vPanelForEnterServerPortAndButton.getChildren().addAll(paneForLabelAndTfEnterAServer,
-                paneForLabelAndTfEnterAPort, paneForButton);
+        vPanelForEnterServerPortAndButton.getChildren().addAll(paneForLabelAndTfEnterAProtocol,
+                paneForLabelAndTfEnterAServer, paneForLabelAndTfEnterAPort, paneForButton);
 
         BorderPane mainPane = new BorderPane();
         // Text area to display contents
@@ -68,9 +86,10 @@ public class Client extends Application {
     }
 
     private void connectToServer() {
-        String server = tfServer.getText();
-        String port = tfPort.getText();
-        if (server.isEmpty() || port.isEmpty()) {
+        server = tfServer.getText();
+        port = tfPort.getText();
+        protocol = tfProtocol.getText();
+        if (server.isEmpty() || port.isEmpty() || protocol.isEmpty()) {
             System.out.println("Please enter the server name and port.");
         } else {
             try (Socket socket = new Socket(server, Integer.parseInt(port))) {
@@ -78,7 +97,7 @@ public class Client extends Application {
                 if (socket.isConnected()) {
                     System.out.println("Connection to server " + server + " is "
                             + socket.isConnected());
-                    findAllPages(server);
+                    findAllPages(protocol, server);
                 } else {
                     System.out.println("Can't connect to the server.");
                 }
@@ -88,17 +107,17 @@ public class Client extends Application {
         }
     }
 
-    private void findAllPages(String startingURL) {
+    private void findAllPages(String protocol, String startingURL) {
         LinkedList<String> listOfPendingURLs = new LinkedList<>();
         HashSet<String> listOfTraversedURLs = new HashSet<>();
 
-        listOfPendingURLs.add("https://" + startingURL);
-        System.out.println(getSizeOfURL("https://" + startingURL));
+        listOfPendingURLs.add(protocol + startingURL);
+        System.out.println(getSizeOfURL(protocol + startingURL));
         while (!listOfPendingURLs.isEmpty() && listOfTraversedURLs.size() <= 100) {
             String url = listOfPendingURLs.pop();
             if (!listOfTraversedURLs.contains(url)) {
                 listOfTraversedURLs.add(url);
-                System.out.println("Craw " + url);
+                System.out.println("Craw " + url + " " + getSizeOfURL(url) + " " + listOfPendingURLs.size());
                 getSubURLs(url).stream().filter(
                         s -> !listOfTraversedURLs.contains(s)).forEach(listOfPendingURLs::push);
 
@@ -121,8 +140,7 @@ public class Client extends Application {
             final byte[] utf8Bytes = inputText.toString().getBytes("UTF-8");
             len = utf8Bytes.length;
             in.close();
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             System.out.println(ex.toString());
         }
 
@@ -130,7 +148,9 @@ public class Client extends Application {
     }
 
     private ArrayList<String> getSubURLs(String urlString) {
-        ArrayList<String> list = new ArrayList<>(1000);
+        //ArrayList<String> list = new ArrayList<>(1000);
+        System.out.println("getSubURLs method work");
+        list.clear();
 
         try {
             URL url = new URL(urlString);
@@ -138,12 +158,15 @@ public class Client extends Application {
             int current = 0;
             while (input.hasNext()) {
                 String line = input.nextLine();
-                current = line.indexOf("https://www.oracle.com", current);
+                current = line.indexOf(protocol + server, current);
                 while (current > 0) {
                     int endIndex = line.indexOf("\"", current);
                     if (endIndex > 0) { // Ensure that a correct URL is found
-                        list.add(line.substring(current, endIndex));
-                        current = line.indexOf("https://www.oracle.com", endIndex);
+                        String subURL = line.substring(current, endIndex);
+                        if (testURL(p, subURL) && !testURL(pPicture, subURL)) {
+                            list.add(subURL);
+                        }
+                        current = line.indexOf(protocol + server, endIndex);
                     } else
                         current = -1;
                 }
@@ -153,5 +176,10 @@ public class Client extends Application {
         }
 
         return list;
+    }
+
+    private static boolean testURL(Pattern p, String testString) {
+        Matcher m = p.matcher(testString);
+        return m.matches();
     }
 }
